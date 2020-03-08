805f2a2be9dd79031838b8b9eea8745620a181ff
Davy MELINA <davy.melina@gmail.com>
2020-03-08 19:51:54 +0100

    day 5

--- exercises/error_handling/errorsn.rs
+++ exercises/error_handling/errorsn.rs
@@ -15,19 +15,17 @@
 //
 // Execute `rustlings hint errorsn` for hints :)
 
-// I AM NOT DONE
 
 use std::error;
 use std::fmt;
 use std::io;
 
 // PositiveNonzeroInteger is a struct defined below the tests.
-fn read_and_validate(b: &mut dyn io::BufRead) -> Result<PositiveNonzeroInteger, ???> {
+fn read_and_validate(b: &mut dyn io::BufRead) -> Result<PositiveNonzeroInteger, Box<dyn error::Error>> {
     let mut line = String::new();
-    b.read_line(&mut line);
-    let num: i64 = line.trim().parse();
-    let answer = PositiveNonzeroInteger::new(num);
-    answer
+    b.read_line(&mut line)?;
+    let num = line.trim().parse()?;
+    Ok(PositiveNonzeroInteger::new(num)?)
 }
 
 // This is a test helper function that turns a &str into a BufReader.
@@ -66,6 +64,46 @@ fn test_ioerror() {
     assert!(read_and_validate(&mut b).is_err());
     assert_eq!("uh-oh!", read_and_validate(&mut b).unwrap_err().to_string());
 }
+
+/*
+pub enum Result<T, E> {
+    Ok(T),
+    Err(E),
+}
+
+pub enum Option<T> {
+    Some(T),
+    None,
+}
+
+impl Result<T, E> {
+    fn unwrap(&self) -> T {
+        if let Ok(value) = self {
+            value
+        } else {
+            panic!("cannot unwrap blabla")
+        }
+    }
+
+    fn unwrap_err(&self) -> E {
+        if let Err(value) = self {
+            value
+        } else {
+            panic!("cannot unwrap error blabla")
+        }
+    }
+}
+
+impl Option<T> {
+    fn unwrap(&self) -> T {
+        if let Some(value) = self {
+            value
+        } else {
+            panic!("cannot unwrap blabla")
+        }
+    }
+}
+*/
 
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
--- exercises/error_handling/option1.rs
+++ exercises/error_handling/option1.rs
@@ -4,21 +4,53 @@
 // on `None`. Handle this in a more graceful way than calling `unwrap`!
 // Execute `rustlings hint option1` for hints :)
 
-// I AM NOT DONE
-
 pub fn pop_too_much() -> bool {
     let mut list = vec![3];
 
     let last = list.pop().unwrap();
     println!("The last item in the list is {:?}", last);
 
-    let second_to_last = list.pop().unwrap();
+    if let Some(second_to_last) = list.pop() {
+        println!(
+            "The second-to-last item in the list is {:?}",
+            second_to_last
+        );
+    };
+
     println!(
         "The second-to-last item in the list is {:?}",
-        second_to_last
+        list.pop().unwrap_or(0)
     );
     true
 }
+
+/*
+impl Option<T> {
+    fn unwrap(&self) -> T {
+        if let Some(value) = self {
+            value
+        } else {
+            panic!("cannot unwrap blabla")
+        }
+    }
+
+    fn unwrap_or(&self, or_value: T) -> T {
+        if let Some(value) = self {
+            value
+        } else {
+            or_value
+        }
+    }
+}
+*/
+
+/*
+Try using a `match` statement where the arms are `Some(thing)` and `None`.
+Or set a default value to print out if you get `None` by using the
+function `unwrap_or`.
+Or use an `if let` statement on the result of `pop()` to both destructure
+a `Some` value and only print out something if we have a value!
+*/
 
 #[cfg(test)]
 mod tests {
